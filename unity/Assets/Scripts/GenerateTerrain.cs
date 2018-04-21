b7b8e1c3c2962c057786977b7d10e9b7c222731d
GGraca <ggracase@gmail.com>
2018-04-21 12:53:35 +0100

    Added Garbage

---
+++ unity/Assets/Scripts/GenerateTerrain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTerrain : MonoBehaviour {
+
+	public GameObject prefab;
+	public int width = 10;
+	public int height = 10;
+	
+	void Start () {
+		for(int i = -width/2; i < width/2; i++){
+			for(int j = -height/2; j < height/2; j++){
+				Instantiate(prefab, new Vector3(i*(16 + 4),0,j*(16 + 4)), Quaternion.identity);
+			}
+		}
+	}
+	
+}
